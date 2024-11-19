9c8624114f01856af1acf2fc84c395d04f276539
Cooper D'Andilly <151992386+CooperDActor-bytes@users.noreply.github.com>
2024-11-19 21:54:28 +1100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-pwa-cache')
+      .then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/Your Wallet_Files/style.css',
+          '/script.js',
+          // Add more URLs to cache here
+        ]);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
