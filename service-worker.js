c5018cb92849d79a186cbda0e9166e79aa19d65b
Cooper D'Andilly <151992386+CooperDActor-bytes@users.noreply.github.com>
2024-11-19 21:56:18 +1100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('my-pwa-cache')
+    caches.open('LinusWallet')
       .then(cache => {
         return cache.addAll([
           '/',
